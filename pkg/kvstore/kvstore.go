ef21dc6fbf0e7d5938d9a43ae516d115b7a2ca42
le.he <le.he@mintegral.com>
2024-12-04 10:48:47 +0800

    update

--- pkg/kvstore/kvstore.go
+++ pkg/kvstore/kvstore.go
@@ -10,15 +10,9 @@ type KVStore interface {
 }
 
 func NewKVStore(options refx.TypeOptions) (KVStore, error) {
-	// 根据配置创建一个KVStore
 	kvstore, err := refx.New(&options)
 	if err != nil {
 		return nil, err
 	}
 	return kvstore.(KVStore), nil
-}
-
-type Marshaler interface {
-	Marshal(v any) ([]byte, error)
-	Unmarshal(data []byte, v any) error
 }
